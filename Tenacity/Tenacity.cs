7de8a542992257e61dc638ef08ce82daf6c506be
blaxxun <blaxxun@blaxxun.blaxxun>
2021-12-01 18:33:04 +0100

    initial release

---
+++ Tenacity/Tenacity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SkillManager;
+
+namespace Tenacity
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class Tenacity : BaseUnityPlugin
+	{
+		private const string ModName = "Tenacity";
+		private const string ModVersion = "1.0";
+		private const string ModGUID = "org.bepinex.plugins.tenacity";
+
+		public void Awake()
+		{
+			Skill tenacity = new("Tenacity", "tenacity-icon.png");
+			tenacity.Description.English("Reduces damage taken by 0.2% per level.");
+			tenacity.Name.German("Hartnäckigkeit");
+			tenacity.Description.German("Reduziert erlittenen Schaden um 0,2% pro Stufe.");
+			tenacity.Configurable = true;
+			
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			Harmony harmony = new(ModGUID);
+			harmony.PatchAll(assembly);
+		}
+		
+		[HarmonyPatch(typeof(Character), nameof(Character.RPC_Damage))]
+		private class ReduceDamageTaken
+		{
+			[UsedImplicitly]
+			private static void Prefix(Character __instance, HitData hit)
+			{
+				if (__instance is Player player)
+				{
+					player.RaiseSkill("Tenacity", (float) Math.Sqrt(hit.GetTotalDamage()));
+
+					hit.ApplyModifier(1 - player.GetSkillFactor("Tenacity") * 0.2f);
+				}
+			}
+		}
+	}
+}
